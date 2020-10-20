b65f21ac21bba6c31eeca541685fd51d9397b03b
Ken Holston <git.kholston@gmail.com>
2020-10-20 12:34:34 -0400

    add passport authenication

--- app.js
+++ app.js
@@ -26,6 +26,36 @@ app.set('views', __dirname);
 app.set('view engine', 'ejs');
 
 app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
+
+passport.use(
+  new LocalStrategy((username,password,done)=>{
+    User.findOne({username:username},(err,user)=>{
+      if(err){ 
+        return done(err);
+      };
+      if(!user){
+        return done(null,false,{msg: 'Incorrect username'})
+      }
+      if(user.password != password){
+        return done(null,false,{msg: 'Incorrect password'})
+      }
+      return done(null, user);
+    })
+  })
+)
+
+passport.serializeUser(function(user,done){
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id,done){
+  User.findById(id,function(id,done){
+    done(err,user);
+  });
+});
+
+
+
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
